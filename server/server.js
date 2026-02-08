b0dfa683cb1c972e508c6deaaaac25b2ec4d35f3
Siu Chun Lok <loklok031124@gmail.com>
2026-02-07 21:11:52 -0500

    Deploy Backend

--- server/server.js
+++ server/server.js
@@ -33,5 +33,9 @@ app.use("/api/templates", templateRoutes);
 // User Routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
+//const PORT = process.env.PORT || 5001;
+//app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
+
+// fly deploy
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server on ${PORT}`));
